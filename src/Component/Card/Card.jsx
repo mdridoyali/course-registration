159e14dd1d32809f40d4248b52a79fa9b13865dd
mdridoyali <mdridoyislamr@gmail.com>
2023-09-14 22:28:32 +0600

    All Cards and Asidebar are created

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,10 +1,18 @@
 
-import React from 'react';
 
 const Card = () => {
   return (
-    <div>
-      
+    <div className="space-y-5 bg-white rounded-xl p-4">
+      <h2 className="text-blue-500 font-bold ">Credit Hour Remaining 7 hr</h2>
+      <hr></hr>
+      <h2 className="font-bold">Course Name</h2>
+      <ol>
+        <li> Introduction to c programming</li>
+      </ol>
+      <hr></hr>
+      <p className="font-bold text-sm"> Total Credit Hour : 13</p>
+      <hr></hr>
+      <p className="font-bold text-sm" >Total Price : 48000 USD</p>
     </div>
   );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,18 +1,46 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from "../Card/Card";
+
 
 const Home = () => {
+
+  const [allCourses, setAllCourses] = useState([]);
+
+console.log(allCourses)
    
   useEffect(() => {
 fetch('data.json')
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => setAllCourses(data))
   }, [])
 
-  return (
-    <div className="grid grid-cols-2 border w-11/12 mx-auto">
-      <div className=" grid grid-cols-3 border w-9/12">cart</div>
 
-      <div className="   border grid grid-cols-3">aside</div>
+  return (
+    <div className="grid grid-cols-12 gap-5 justify-between w-11/12 mb-5 mx-auto">
+      <div className=" grid grid-cols-3 gap-5 col-span-9">
+        {
+         allCourses.map((course) => 
+         <div key={course.id} className="bg-white rounded-xl p-4 space-y-3">
+              <img src={course.image}/>
+              <h2 className="font-bold">{course.course_name}</h2>
+              <p className="text-sm text-gray-500">{course.course_desc}</p>
+              <div className="flex justify-between">
+                <p>$ Price: {course.price}</p>
+                <p> Credit: {course.credit}hr</p>
+              </div>
+
+              <button className="bg-blue-500 hover:bg-blue-700 py-1 text-white font-bold w-full rounded-lg">Select</button>
+
+            </div>
+         )
+        }
+      </div>
+
+      <div className="col-span-3 ">
+      <Card></Card>
+      </div>
+
     </div>
   );
 };
